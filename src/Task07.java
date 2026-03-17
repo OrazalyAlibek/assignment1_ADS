66d30a51e244ee14977c5d447a8a3cf7c0073594
alibekorazaly <alibek.orazaly1012@gmail.com>
2026-03-17 19:02:09 +0500

    fixed

--- src/Task07.java
+++ src/Task07.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 
 public class Task07 {
-    public static void reverse(int[] arr, int start) {
-        if (start == arr.length) {
+    public static void reverse(int n, Scanner sc) {
+        if (n <= 0) {
             return;
         }
-        reverse(arr, start + 1);
-        System.out.print(arr[start]+" ");
+        int currentNum = sc.nextInt();
+        reverse(n-1,sc);
+        System.out.print(currentNum + " ");
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr  = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            reverse(n, sc);
         }
-        reverse(arr, 0);
     }
-
 }
